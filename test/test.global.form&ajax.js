90fa2d1424a22154c6c9947aee3c695b45b939cd
Jüri Kormik <jyri78@users.noreply.github.com>
2023-06-14 13:28:03 +0300

    Some changes in fd(); new test cases

--- test/test.global.form&ajax.js
+++ test/test.global.form&ajax.js
@@ -5,6 +5,9 @@ describe('GLOBAL: form & ajax', () => {
 
 
     describe('$fd', () => {
+        it('should throw Error', () => {
+            assert.throws( $fd, Error, 'Form not found.' );
+        });
         it('should return NULL and set classes accordingly', () => {
             let validation = ['is-valid', 'is-valid', 'is-valid', 'is-invalid', 'is-invalid'],
                 required = ['test-field-1', 'test-field-3'],
@@ -16,10 +19,22 @@ describe('GLOBAL: form & ajax', () => {
             });
  
             // Get form data and confirm set classes accordingly
-            assert.isNull( $fd('test-form', required, {}, 'json', classes) );
+            assert.isNull( $fd('test-form-1', 'json', required, {}, classes) );
             validation.forEach((cl, idx) => {
                 assert.isTrue( $hcl(`test-field-${idx}`, cl) );
             });
+        });
+        it('should return JSON object', () => {
+            let data = $fd('test-form-2', 'json');
+ 
+            assert.isObject( data );
+            assert.hasAllKeys( data, ['testField5', 'testField6'] );
+            assert.deepEqual( data, {testField5: 'textual value', testField6: 7} );
+        });
+        it('should return string (URLSearchParams)', () => {
+            let data = $fd('test-form-2', 'string');
+ 
+            assert.strictEqual( data, 'test-field-5=textual+value&test-field-6=7' );
         });
     });
 
